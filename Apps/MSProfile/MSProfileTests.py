b77baac5e5118849da2857cdf11481b657c3c89a
mohamedhaf4050 <mohamed.hafez4050@gmail.com>
2023-07-01 22:44:14 +0300

    AllMSProfileTestsRunning
    Co-authored-by: Hosam K  <HosamKsbaa@users.noreply.github.com>

--- Apps/MSProfile/MSProfileTests.py
+++ Apps/MSProfile/MSProfileTests.py
@@ -4,6 +4,7 @@ from .MSProfileApp import app
 
 client = TestClient(app)
 
+
 def test_create_user_profile_success():
     # Send a request to create a new user profile
     payload = {
@@ -19,6 +20,11 @@ def test_create_user_profile_success():
 
     # Assert the response body contains the correct user ID
     assert response.json() == {"userId": '123'}
+
+    response = client.delete("/api/profiles/123?user_id=123")
+
+    
+    
 
 
 def test_create_user_profile_existing_id():
@@ -46,6 +52,8 @@ def test_create_user_profile_existing_id():
     # Assert the response body contains the correct error message
     assert response.json() == {"detail": "User ID already exists"}
 
+    
+
 def test_create_user_profile_invalid_data():
     # Send a request to create a user profile with invalid data
     payload = {
@@ -60,38 +68,38 @@ def test_create_user_profile_invalid_data():
     assert response.status_code == 400
 
 
-# def test_delete_user_profile_success():
-#     # Create a user profile for deletion
-#     profile = {
-#         "userId": "789",
-#         "username": "john_doe",
-#         "email": "john.doe@example.com",
-#         "profilePictureUrl": "https://example.com/profile.jpg"
-#     }
+def test_delete_user_profile_success():
+    # Create a user profile for deletion
+    profile = {
+        "userId": "789",
+        "username": "john_doe",
+        "email": "john.doe@example.com",
+        "profilePictureUrl": "https://example.com/profile.jpg"
+    }
 
-#     # Insert the profile into the database
-#     response = client.post("/api/profiles", json=profile)
+    # Insert the profile into the database
+    response = client.post("/api/profiles", json=profile)
 
-#     # Send a request to delete the user profile
-#     response = client.delete("/api/profiles/789")
+    # Send a request to delete the user profile
+    response = client.delete("/api/profiles/123?user_id=789")
 
-#     # Assert the response status code is 200 (OK)
-#     assert response.status_code == 200
+    # Assert the response status code is 200 (OK)
+    assert response.status_code == 200
 
-#     # Assert the response body contains the correct message
-#     assert response.json() == {"message": "User profile deleted"}
+    # Assert the response body contains the correct message
+    assert response.json() == {"message": "User profile deleted"}
 
 
 
-# def test_delete_user_profile_not_found():
-#     # Send a request to delete a user profile that doesn't exist
-#     response = client.delete("/api/profiles/99999")
+def test_delete_user_profile_not_found():
+    # Send a request to delete a user profile that doesn't exist
+    response = client.delete("/api/profiles/99999?user_id=99999")
 
-#     # Assert the response status code is 422
-#     assert response.status_code == 422
+    # Assert the response status code is 422
+    assert response.status_code == 404
 
-#     # Assert the response body contains the correct error message
-#     assert response.json() == {"detail": "User not found"}
+    # Assert the response body contains the correct error message
+    assert response.json() == {"detail": "User not found"}
 
 
 
