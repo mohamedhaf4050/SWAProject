51c44088074ac57abe00c0755c4480992c64e2fe
hosam ksbaa <Hosamksbaa@gmail.com>
2023-07-04 03:08:05 +0300

    asdsds
    Co-authored-by: mohamedhaf4050 <mohamedhaf4050@users.noreply.github.com>

--- Apps/MSModule/MSModuleApp.py
+++ Apps/MSModule/MSModuleApp.py
@@ -48,6 +48,7 @@ from confluent_kafka import KafkaException
 import json
 from confluent_kafka import Producer
 import os
+from ..Util.kafka import publish_to_kafka, create_topic, kafka_conf, kafka_producer
 
 
 # Connect to MongoDB
@@ -56,39 +57,39 @@ db = client["elearningDB"]
 collection = db["modules"]
 
 # Kafka configuration
-kafka_bootstrap_servers =  os.getenv('KAFKA_BOOTSTRAP_SERVERS')
-kafka_topic = "elearning_topic"
+# kafka_bootstrap_servers =  os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+# kafka_topic = "elearning_topic"
 
-# Kafka producer configuration
-kafka_conf = {
-    "bootstrap.servers":  os.getenv('KAFKA_BOOTSTRAP_SERVERS'),  # Kafka broker address
-    "client.id": "user-profile-producer",  # Unique ID for the Kafka producer
-}
-kafka_producer = Producer(kafka_conf)
+# # Kafka producer configuration
+# kafka_conf = {
+#     "bootstrap.servers":  os.getenv('KAFKA_BOOTSTRAP_SERVERS'),  # Kafka broker address
+#     "client.id": "user-profile-producer",  # Unique ID for the Kafka producer
+# }
+# kafka_producer = Producer(kafka_conf)
 
-def publish_to_kafka(topic: str, message: dict):
-    try:
-        value = json.dumps(message).encode("utf-8")
-        kafka_producer.produce(topic, value=value)
-        kafka_producer.flush()
-    except KafkaException as e:
-        if e.args[0].code() == KafkaException.UNKNOWN_TOPIC_OR_PART:
-            create_topic(topic)
-            kafka_producer.produce(topic, value=value)
-            kafka_producer.flush()
-        else:
-            raise e
-#aaa
-#hello
+# def publish_to_kafka(topic: str, message: dict):
+#     try:
+#         value = json.dumps(message).encode("utf-8")
+#         kafka_producer.produce(topic, value=value)
+#         kafka_producer.flush()
+#     except KafkaException as e:
+#         if e.args[0].code() == KafkaException.UNKNOWN_TOPIC_OR_PART:
+#             create_topic(topic)
+#             kafka_producer.produce(topic, value=value)
+#             kafka_producer.flush()
+#         else:
+#             raise e
+# #aaa
+# #hello
 
 
-def create_topic(topic: str):
-    admin_client = AdminClient({"bootstrap.servers": kafka_bootstrap_servers})
-    topic_metadata = admin_client.list_topics(timeout=5)
-    if topic not in topic_metadata.topics:
-        new_topic = NewTopic(topic, num_partitions=1, replication_factor=1)
-        admin_client.create_topics([new_topic], request_timeout=15)
-        admin_client.close()
+# def create_topic(topic: str):
+#     admin_client = AdminClient({"bootstrap.servers": kafka_bootstrap_servers})
+#     topic_metadata = admin_client.list_topics(timeout=5)
+#     if topic not in topic_metadata.topics:
+#         new_topic = NewTopic(topic, num_partitions=1, replication_factor=1)
+#         admin_client.create_topics([new_topic], request_timeout=15)
+#         admin_client.close()
 
 
 
